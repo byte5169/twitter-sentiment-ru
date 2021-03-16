e2eaae7be8c8ee79252c25e0a1566b54b8d667dd
byte5169 <irshenko@gmail.com>
2021-03-16 12:03:09 +0300

    bot v1.0

--- bot/bot.py
+++ bot/bot.py
@@ -4,107 +4,90 @@ To import 'mymodule' that is in the parent directory of your current module with
 """
 import os, sys, inspect
 
-from six import text_type
-
 currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 parentdir = os.path.dirname(currentdir)
 sys.path.insert(0, parentdir)
 
-import logging
-import telebot
-import pandas as pd
 
-from processing.sentiment import get_sentiment_string
-from processing.data import get_tweets, clean_txt
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from processing.sentiment import get_sentiment_list, get_sentiment_string
+
+
+"""
+Credentials
+"""
 from dotenv import load_dotenv
 
-logger = telebot.logger
-telebot.logger.setLevel(logging.DEBUG)
 load_dotenv()
 bot_token = os.getenv("bot_token")
-bot = telebot.TeleBot(bot_token, parse_mode=None)
-
 
 """
-Handling start of the bot
+Logging
 """
-commands = {
-    "help": "Gives you information about the available commands",
-    "sentiment": "Gives you 10 latest tweets analyzed from user",
-}
+import logging
+
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
+logger = logging.getLogger(__name__)
+
+"""
+Handling unknown commands
+"""
 
 
-@bot.message_handler(commands=["start"])
-def command_start(m):
-    cid = m.chat.id
-    bot.send_message(
-        cid,
-        "Hello! \nType /help for all available commands.",
+def unknown(update, context):
+    context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text="Sorry, I didn't understand that command.",
     )
 
 
-@bot.message_handler(commands=["help"])
-def command_help(m):
-    cid = m.chat.id
-    help_text = "The following commands are available: \n"
-    for key in commands:
-        help_text += "/" + key + ": "
-        help_text += commands[key] + "\n"
-    bot.send_message(cid, help_text)
+"""
+Start command
+"""
+
+
+def start(update, context):
+    context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text="""Please type /text followed by a text to get sentiment for that line. Example "/text Привет ублюдок!" """,
+    )
 
 
 """
-Handling tweets sentiment
+text_sentiment_handler
 """
-name = ""
-number_tweets = 5
 
 
-@bot.message_handler(content_types=["text"])
-def twitter_sentiment(m):
-    cid = m.chat.id
-    if m.text == "/tweets":
-        bot.send_message(cid, "Please enter twitter account name.")
-        bot.register_next_step_handler(m, handle_tweet_count)
-    else:
-        bot.send_message(cid, "Please type /tweets")
+def text_sentiment(update, context):
+    line = " ".join(context.args)
+    sentiment_line = get_sentiment_string(line)
+    text = f"Original: \n {line} \n Results: \n {sentiment_line}"
+    context.bot.send_message(chat_id=update.effective_chat.id, text=text)
 
 
-def handle_tweet_count(m):
-    global name
-    name = m.text
-    cid = m.chat.id
-    bot.send_message(cid, "Please enter number of tweets you want to see.")
-    bot.register_next_step_handler(m, handle_twitter_info)
+"""
+Main function
+"""
 
 
-def handle_twitter_info(m):
-    global number_tweets
-    number_tweets = m.text
-    cid = m.chat.id
-    try:
-        df = get_tweets(name, tweet_count=number_tweets, lang="ru")
-        all_tweets = df["Tweets"].to_list()
+def main() -> None:
+    # Create the Updater and pass it your bot's token.
+    updater = Updater(token=bot_token, use_context=True)
+    dispatcher = updater.dispatcher
 
-        for tweet in all_tweets:
-            clean_tweet = clean_txt(tweet)
-            sentiment_tweet = get_sentiment_string(clean_tweet)
-            for sentiment in sentiment_tweet:
-                bot.send_message(cid, f"{clean_tweet} \n Results: \n {sentiment}")
+    dispatcher.add_handler(CommandHandler("start", start))
+    dispatcher.add_handler(CommandHandler("text", text_sentiment))
+    dispatcher.add_handler(MessageHandler(Filters.command, unknown))
 
-    except Exception:
-        bot.send_message(cid, "Account doesn't exist, please enter the correct one.")
-        bot.register_next_step_handler(m, handle_tweet_count)
+    # Bot start/stop
+    updater.start_polling()
+    updater.idle()
 
 
-bot.polling()
+if __name__ == "__main__":
+    main()
 
 
-### FEATURES
-# limit latest number of tweets
-# clean up sentiment output
-# decide how to choose a tweet for analyze
-# add sentiment for random text
-
-### BUGS
-# currently there is a bug, you need to enter correct number twice
+# https://github.com/python-telegram-bot/python-telegram-bot/tree/master/examples
